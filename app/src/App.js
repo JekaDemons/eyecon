3ebebebb939f96a28e29aacf65e941df31ce62c8
Ido Calman <calman.ido@gmail.com>
2021-10-06 21:37:48 -0400

    Adds FaceAPI models

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,31 @@
 import React, { useState } from 'react';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+import Webcam from "react-webcam";
 import './App.css';
 
+const client = new W3CWebSocket('ws://127.0.0.1:8080', 'echo-protocol')
+
+client.onerror = function() {
+    console.log('Connection Error');
+};
+
+client.onopen = function() {
+    console.log('WebSocket Client Connected');
+
+    function sendNumber() {
+        if (client.readyState === client.OPEN) {
+            var number = Math.round(Math.random() * 0xFFFFFF);
+            client.send(number.toString());
+            console.log('sent number', number);
+            setTimeout(sendNumber, 1000);
+        }
+    }
+    sendNumber();
+};
+
+function onUserMedia(e) {
+    console.log(e)
+}
 
 const defaultParticipants = [
     {
@@ -68,6 +93,7 @@ function calcAllPoses(participants, gaze, setParticipants) {
 }
 
 function App() {
+    console.log(faceapi)
     const [participants, setParticipants] = useState(defaultParticipants)
     const [pointer, setPointer] = useState({"x": 0, "y": 0})
     return (
